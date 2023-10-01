b46fc42d9e7b386be0728a29f06cfeeb3bf2854b
NUTAD <86157431+weiweiwei8878dayo@users.noreply.github.com>
2023-10-01 20:35:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
     const messageInput = document.getElementById('message-input');
     const sendButton = document.getElementById('send-button');
 
+    // メッセージをローカルストレージから読み込む
+    const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+
+    // ローカルストレージから読み込んだメッセージを表示する
+    storedMessages.forEach(function (messageText) {
+        const messageElement = document.createElement('div');
+        messageElement.textContent = messageText;
+        chatLog.appendChild(messageElement);
+    });
+
     sendButton.addEventListener('click', function () {
         const messageText = messageInput.value;
         if (messageText.trim() !== '') {
             const messageElement = document.createElement('div');
             messageElement.textContent = messageText;
             chatLog.appendChild(messageElement);
+
+            // メッセージをローカルストレージに保存
+            storedMessages.push(messageText);
+            localStorage.setItem('chatMessages', JSON.stringify(storedMessages));
+
             messageInput.value = '';
         }
     });
