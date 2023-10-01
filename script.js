7f011350c0324a36fc57c867d44b01addbf64f3e
NUTAD <86157431+weiweiwei8878dayo@users.noreply.github.com>
2023-10-01 20:45:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
+// Firebase構成情報をセットアップ
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Firebaseプロジェクトに接続
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
 document.addEventListener('DOMContentLoaded', function () {
     const chatLog = document.getElementById('chat-log');
     const messageInput = document.getElementById('message-input');
     const sendButton = document.getElementById('send-button');
 
-    // メッセージをローカルストレージから読み込む
-    const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
-
-    // ローカルストレージから読み込んだメッセージを表示する
-    storedMessages.forEach(function (messageText) {
-        const messageElement = document.createElement('div');
-        messageElement.textContent = messageText;
-        chatLog.appendChild(messageElement);
-    });
-
     sendButton.addEventListener('click', function () {
         const messageText = messageInput.value;
         if (messageText.trim() !== '') {
-            const messageElement = document.createElement('div');
-            messageElement.textContent = messageText;
-            chatLog.appendChild(messageElement);
-
-            // メッセージをローカルストレージに保存
-            storedMessages.push(messageText);
-            localStorage.setItem('chatMessages', JSON.stringify(storedMessages));
-
+            // Firebase Realtime Databaseにメッセージを保存
+            database.ref('chat').push({
+                message: messageText
+            });
             messageInput.value = '';
         }
     });
 
-    messageInput.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            sendButton.click();
-        }
+    // Firebase Realtime Databaseからメッセージをリアルタイムで取得
+    database.ref('chat').on('child_added', function (snapshot) {
+        const messageText = snapshot.val().message;
+        const messageElement = document.createElement('div');
+        messageElement.textContent = messageText;
+        chatLog.appendChild(messageElement);
     });
 });
